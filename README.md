c27efda69e93ad9a4aba64fb5e79c21201ef225a
i3games <i3games@users.noreply.github.com>
2020-03-21 10:37:15 +0100

    get segmentation data

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # dont-touch-men
 
-Uses hand and face detection to alert people before they are touching MEN (Mouth Eyes Nose).
+Alert people before they are touching MEN (Mouth Eyes Nose).
+
+WORK IN PROGRESS, I'M PROTOTYPING ALONG, CURRENT STATUS: DOES NOT WORK YET
+
+https://i3games.github.io/dont-touch-men/
+
+
 
 In the current COVID-19 pandemic, a few rules have been communicated to reduce infections. 
 
@@ -20,6 +26,13 @@ To be clear. Do NOT do this:
 
 https://www.youtube.com/watch?v=IgR6uaVqWsQ
 
-Over the last couple of days, during videoconferences and on social media I have seen people touching their faces all the time. It happens subconsciously. Even if you tell people not to do it they will have their hand in their face a minute later.
+Over the last couple of days, during videoconferences and on social media I have seen people constantly touching their faces. It happens subconsciously. Even if you tell people not to do it they will have their hand in their face a minute later. And so will you. This way a virus gets on a mucous membrane of yours and enters your body. 
 
-The goal is to prototype a little technical helper that gives a warning if your hand comes close to your head.
+The goal is to prototype a technical helper that gives a warning if your hand comes close to your head.
+
+
+Uses the following libraries / frameworks
+
+* p5.js: https://github.com/processing/p5.js
+* ml5.js: https://github.com/ml5js/ml5-library
+* bodypix: https://github.com/tensorflow/tfjs-models/tree/master/body-pix
--- sketch.js
+++ sketch.js
@@ -1,14 +1,18 @@
 let bodypix;
 let video;
 let segmentation;
+let hand = 0, face = 0;
+let clicked = false;
 
 const options = {
-    outputStride: 8, // 8, 16, or 32, default is 16
-    segmentationThreshold: 0.5, // 0 - 1, defaults to 0.5 
+    architecture: 'MobileNetV1',
+    multiplier: 0.75, // 0 - 1, defaults to 0.75, higher is slower / more accurate
+    outputStride: 16, // 8, 16, or 32, default is 16, higher is faster / less accurate
+    segmentationThreshold: 0.75, // 0 - 1, defaults to 0.5 
 }
 
 function preload(){
-    bodypix = ml5.bodyPix(options)
+    bodypix = ml5.bodyPix(options);
 }
 
 function setup() {
@@ -16,31 +20,58 @@ function setup() {
     video = createCapture(VIDEO);
     video.size(width, height);
     video.hide();
-    
-    options.palette = bodypix.config.palette;
-    Object.keys(options.palette).forEach(part => {
-        const r = floor(random(255));
-        const g = floor(random(255));
-        const b = floor(random(255));
-        options.palette[part].color = [r, g, b]
-    });
 
-    bodypix.segmentWithParts(video, gotResults, options)
-
+    bodypix.segmentWithParts(video, gotResults, options);
 }
 
-function gotResults(err, result) {
-    if (err) {
-        console.log(err)
-        return
+function touchStarted() { clicked = true; }
+function mousePressed()  { clicked = true; }
+
+function gotResults(err, segmentation) {
+    if (err) { 
+        console.log(err);
+        return;
     }
-    segmentation = result;
 
+    // console.log(segmentation.segmentation.data);
+    const leftHandId = segmentation.bodyParts.leftHand.id;
+    const rightHandId = segmentation.bodyParts.rightHand.id;
+    const leftFaceId = segmentation.bodyParts.leftFace.id;
+    const rightFaceId = segmentation.bodyParts.rightFace.id;
+    
+    const h = segmentation.raw.partMask.height;
+    const w = segmentation.raw.partMask.width;
+    const current = segmentation.segmentation.data;
+    // appears to be a BUG, dimensions dont match, the array should be of size w * h
+    // console.log(w, h, current.length, clicked);
+
+    hand = 0;
+    face = 0;
+    
+    for (let i of current) {
+        // console.log(i);
+
+        // false positives galore 
+        if (i == leftHandId || i == rightHandId) {
+            hand++;
+        }
+        if (i == leftFaceId || i == rightFaceId) {
+            face++;
+        }
+        
+    }
+    
+    console.log(hand, face);
     background(255, 0, 0);
-    // image(video, 0, 0, width, height)
-    image(segmentation.partMask, 0, 0, width, height)
+    // image(video, 0, 0, width, height);
+    image(segmentation.partMask, 0, 0, width, height);
 
-    bodypix.segmentWithParts(video, gotResults, options)
+    if (!clicked) { // might need up to 2 clicks to start, focus the canvas and user interaction
+        textAlign(CENTER, CENTER);
+        textSize(width / 8);
+        text('tap/click twice', width/2, height/2);
+    }
 
+    bodypix.segmentWithParts(video, gotResults, options);
 }
 
