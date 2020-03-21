1479d9d779230a59363d779191943aa5fe03f903
i3games <i3games@users.noreply.github.com>
2020-03-21 19:04:30 +0100

    partial crude detection

--- README.md
+++ README.md
@@ -2,11 +2,9 @@
 
 Alert people before they are touching MEN (Mouth Eyes Nose).
 
-WORK IN PROGRESS, I'M PROTOTYPING ALONG, CURRENT STATUS: DOES NOT WORK YET
+CURRENT STATUS: WORK IN PROGRESS, TESTING NEEDED
 
 https://i3games.github.io/dont-touch-men/
-
-
 
 In the current COVID-19 pandemic, a few rules have been communicated to reduce infections. 
 
@@ -20,16 +18,11 @@ In the current COVID-19 pandemic, a few rules have been communicated to reduce i
 8. Don't hoard.
 9. Be excellent to each other.
 
-Here I want to focus on 3. 
+Over the last couple of days, during videoconferences and on social media I have seen people constantly touching their faces. It happens subconsciously. Even if you tell people not to do it they will have their hand in their face a minute later. And so will you. This way the virus gets on a mucous membrane of yours and enters your body. 
 
-To be clear. Do NOT do this:
+To be clear. Do NOT do this: https://www.youtube.com/watch?v=IgR6uaVqWsQ
 
-https://www.youtube.com/watch?v=IgR6uaVqWsQ
-
-Over the last couple of days, during videoconferences and on social media I have seen people constantly touching their faces. It happens subconsciously. Even if you tell people not to do it they will have their hand in their face a minute later. And so will you. This way a virus gets on a mucous membrane of yours and enters your body. 
-
-The goal is to prototype a technical helper that gives a warning if your hand comes close to your head.
-
+The goal is to prototype a minimal helper that gives you a warning if your hand comes close to your face.
 
 Uses the following libraries / frameworks
 
--- sketch.js
+++ sketch.js
@@ -1,8 +1,7 @@
-let bodypix;
-let video;
-let segmentation;
-let hand = 0, face = 0;
-let clicked = false;
+
+const handThreshold = 100; // minimum amount of pixels of type hand visible
+const faceThreshold = 100; // minimum amount of pixels of type face visible
+const radius = 5; // radius in pixels around the hand pixel sampled to look for face pixels 
 
 const options = {
     architecture: 'MobileNetV1',
@@ -10,6 +9,12 @@ const options = {
     outputStride: 16, // 8, 16, or 32, default is 16, higher is faster / less accurate
     segmentationThreshold: 0.75, // 0 - 1, defaults to 0.5 
 }
+
+let bodypix;
+let video;
+let segmentation;
+let hand = 0, face = 0;
+let clicked = false;
 
 function preload(){
     bodypix = ml5.bodyPix(options);
@@ -20,12 +25,16 @@ function setup() {
     video = createCapture(VIDEO);
     video.size(width, height);
     video.hide();
-
     bodypix.segmentWithParts(video, gotResults, options);
+}
+
+function alarm() {
+    console.log("alarm");
 }
 
 function touchStarted() { clicked = true; }
 function mousePressed()  { clicked = true; }
+
 
 function gotResults(err, segmentation) {
     if (err) { 
@@ -33,35 +42,46 @@ function gotResults(err, segmentation) {
         return;
     }
 
-    // console.log(segmentation.segmentation.data);
     const leftHandId = segmentation.bodyParts.leftHand.id;
     const rightHandId = segmentation.bodyParts.rightHand.id;
     const leftFaceId = segmentation.bodyParts.leftFace.id;
     const rightFaceId = segmentation.bodyParts.rightFace.id;
     
+    function isFace (pixel) { return pixel == leftHandId || pixel == rightHandId; } 
+    function isHand (pixel) {  return pixel == leftFaceId || pixel == rightFaceId; } 
+  
     const h = segmentation.raw.partMask.height;
     const w = segmentation.raw.partMask.width;
+    // console.log(segmentation.segmentation.data);
     const current = segmentation.segmentation.data;
-    // appears to be a BUG, dimensions dont match, the array should be of size w * h
+    // appears to be a BUG, the segmentsation data array should be of size w * h
     // console.log(w, h, current.length, clicked);
 
     hand = 0;
     face = 0;
-    
-    for (let i of current) {
-        // console.log(i);
 
-        // false positives galore 
-        if (i == leftHandId || i == rightHandId) {
+    for (const [i, v] of current.entries()) {
+        if (v == leftHandId || v == rightHandId) {
             hand++;
+            if (hand > handThreshold && face > faceThreshold) { // minimum amount of hand / face pixels
+                
+                let left = current[i-radius];
+                let right = current[i+radius];
+                // let above = current[i-radius];   // tbd, see BUG above
+                // let bellow = current[i-radius];  // tbd, see BUG above
+                
+                if ((left && isFace(left))  || (right && isFace(right))) {
+                    alarm();
+                    break;
+                }
+            }
+    
         }
-        if (i == leftFaceId || i == rightFaceId) {
+        if (v == leftFaceId || v == rightFaceId) {
             face++;
-        }
-        
+        }        
     }
     
-    console.log(hand, face);
     background(255, 0, 0);
     // image(video, 0, 0, width, height);
     image(segmentation.partMask, 0, 0, width, height);
